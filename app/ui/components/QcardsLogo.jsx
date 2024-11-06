e047ad45bdf306ffd30683c2dac8093fe8a253ee
filira2004 <grobovahik11119@mail.ru>
2024-11-06 18:50:34 +0400

    uninstall heroicons and add svg

--- app/ui/components/QcardsLogo.jsx
+++ app/ui/components/QcardsLogo.jsx
@@ -1,9 +1,21 @@
-import { Square2StackIcon } from "@heroicons/react/24/outline";
-
 const QcardsLogo = () => {
   return (
     <div className="flex items-center  text-[#050505] select-none">
-      <Square2StackIcon className="w-9 h-9 rotate-[5deg]" />
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className="size-9 rotate-[5deg]"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M16.5 8.25V6a2.25 2.25 0 0 0-2.25-2.25H6A2.25 2.25 0 0 0 3.75 6v8.25A2.25 2.25 0 0 0 6 16.5h2.25m8.25-8.25H18a2.25 2.25 0 0 1 2.25 2.25V18A2.25 2.25 0 0 1 18 20.25h-7.5A2.25 2.25 0 0 1 8.25 18v-1.5m8.25-8.25h-6a2.25 2.25 0 0 0-2.25 2.25v6"
+        />
+      </svg>
+
       <p className="font-bold tracking-tighter text-xl">QCards</p>
     </div>
   );
