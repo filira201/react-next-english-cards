f7319a85744ba408cf3c7b60159365ddbc4c5622
filira2004 <grobovahik11119@mail.ru>
2024-11-23 14:00:49 +0400

    add metadata

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,37 @@
 import "@/app/ui/globals.css";
 import { inter } from "./ui/fonts";
 
+export const metadata = {
+  title: {
+    template: "%s | QCards",
+    default: "QCards",
+  },
+  description:
+    "QCards это удобный сервис для создания флеш-карточек и прохождения викторин. Улучшайте знания и память с помощью интерактивных карточек для учебы, работы и досуга.",
+  icons: {
+    icon: [
+      {
+        rel: "icon",
+        url: "/favicon-16x16.png",
+        sizes: "16x16",
+        type: "image/png",
+      },
+      {
+        rel: "icon",
+        url: "/favicon-32x32.png",
+        sizes: "32x32",
+        type: "image/png",
+      },
+    ],
+    apple: "/apple-touch-icon.png",
+    shortcut: "/favicon.ico",
+    other: [
+      { rel: "android-chrome-192x192", url: "/android-chrome-192x192.png" },
+      { rel: "android-chrome-512x512", url: "/android-chrome-512x512.png" },
+    ],
+  },
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
